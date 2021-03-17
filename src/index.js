b934703040db6715acdddf81c0cdef267f33d196
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-03-17 12:46:56 -0400

    Linked jQuery and made more progress. Made some good progress.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import "./styles.css";
 
+let results = [];
+let random;
 
+$("#heads").hover(function () {
+  $("#p").html("Click to choose Heads.");
+});
+
+$("#tails").hover(function () {
+  $("#p").html("Click to choose Tails.");
+});
+// function randomFunc() {
+//   random = Math.floor(Math.random() * 10);
+// }
+
+$("#heads").click(function () {
+  random = Math.floor(Math.random() * 10);
+  if (random <= 4) {
+    $("#result").html("");
+  }
+});
+
+$("#tails").click(function () {});

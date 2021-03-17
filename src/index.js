5483b347fe78392daaf7deca76b2e2b5a831f84e
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-03-17 13:38:38 -0400

    If statements dont work completely. I will have to fix them later.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles.css";
 
 let results = [];
 let random;
+let result;
+let userChoice;
 
 $("#heads").hover(function () {
   $("#p").html("Click to choose Heads.");
@@ -10,15 +12,34 @@ $("#heads").hover(function () {
 $("#tails").hover(function () {
   $("#p").html("Click to choose Tails.");
 });
-// function randomFunc() {
-//   random = Math.floor(Math.random() * 10);
-// }
 
-$("#heads").click(function () {
+function randomFunc() {
   random = Math.floor(Math.random() * 10);
   if (random <= 4) {
-    $("#result").html("");
+    result = "heads";
   }
+  else if (random >= 5){
+    result = "tails"
+  }
+}
+function evaluate (choice){
+  if (choice = result){
+    $("#result").html('You win!');
+  }
+  else{
+    $("#result").html('You lose!');
+  }
+}
+
+
+$("#heads").click(function () {
+  userChoice = "heads";
+  randomFunc();
+  evaluate(userChoice);
 });
 
-$("#tails").click(function () {});
+$("#tails").click(function () {
+  userChoice = "tails";
+  randomFunc();
+  evaluate(userChoice);
+});
